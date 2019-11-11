f8174a6458c7320eb601d1a5fcd1f490f2aec461
Jacob Veatch <jakedveatch@gmail.com>
2019-11-11 15:30:03 -0600

    It finally built on an emulator

---
+++ Blackout/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp91a9299c4c964134a18b9f12dd3d8ede://192.168.1.13:19000";
+
+}
