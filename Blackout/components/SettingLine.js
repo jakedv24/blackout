645031bf4a31b4e743e0b6bca40830240db960cc
ajuarez99 <allanjuarez86@gmail.com>
2019-11-13 14:55:34 -0600

    get simple component to work and not break app

--- Blackout/components/SettingLine.js
+++ Blackout/components/SettingLine.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
 import { View, FlatList } from "react-native";
 import { connect } from "react-redux";
+import TrackingToggle from "./header/TrackingToggle";
+import { StyleSheet, Text } from "react-native";
 
 class SettingLine extends Component {
   state = {};
+  render() {
+    const { sectionTitle } = this.props;
+    return (
+      <View>
+        <TrackingToggle />
+      </View>
+    );
+  }
 }
+export default SettingLine;
+const styles = StyleSheet.create({
+  settingName: {
+    fontWeight: "bold"
+  }
+});
--- Blackout/screens/SettingsScreen.js
+++ Blackout/screens/SettingsScreen.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import { ExpoConfigView } from "@expo/samples";
+import SettingLine from "../components/SettingLine";
+import HorizontalRule from "../components/HorizontalRule";
 
 export default function SettingsScreen() {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  return <SettingLine />;
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: "Setting"
 };
