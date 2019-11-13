e0fb23ea963c8886c0a9e30468e8ce7e64e266a0
ajuarez99 <allanjuarez86@gmail.com>
2019-11-13 14:38:09 -0600

    added settingline page

---
+++ Blackout/components/SettingLine.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
+import { connect } from "react-redux";
+
+class SettingLine extends Component {
+  state = {};
+}
